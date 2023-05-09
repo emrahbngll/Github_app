c25b007dceadf52c57aa11114020782b1e45ed5e
emrahbnglll <gscimbommm842@gmail.com>
2023-05-09 12:10:14 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1>GITHUB APP </h1>
+<p> ReactJs | Material-Ui | React-Router  |Redux| Css|React-Router |
+Json-Server Used To develope this app  <p> 
+<p>Building a project using React-Redux,React-Router with Css<p>
+<img src= "./public/Github-app.gif" />
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { GithubState } from './context/GithubContext';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './App.css';
+import Hero from './components/Hero';
+
+function App() {
+  return (
+    <GithubState>
+      <Router>
+        <Hero />
+      </Router>
+    </GithubState>
+  );
+}
+
+export default App;
